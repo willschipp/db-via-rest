6ef41db2789ffe43a2d136b9b111f9e284a7991c
will <will@wills-Mac-Pro.local>
2020-08-03 16:56:22 -0500

    sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into person (id, last_name, first_name, last_updated_timestamp) values (1,'Doe','John',current_timestamp());
+insert into person (id, last_name, first_name, last_updated_timestamp) values (2,'Doe','Jane',current_timestamp());
+insert into person (id, last_name, first_name, last_updated_timestamp) values (3,'Smith','Jack',current_timestamp());
+insert into person (id, last_name, first_name, last_updated_timestamp) values (4,'Brown','Jill',current_timestamp());
+insert into person (id, last_name, first_name, last_updated_timestamp) values (5,'Citizen','John',current_timestamp());
